9e633e15a13aeb004444cd003b0287130131efcb
enisc-al <80548505+enisc-al@users.noreply.github.com>
2022-05-09 12:42:55 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,8 +1,3 @@
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
